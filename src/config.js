e4a71072900d9ce61c98aa76d12180d62c8922a4
tickbh <tickdream125@hotmail.com>
2016-12-16 17:19:21 +0800

    protocol

--- src/config.js
+++ src/config.js
@@ -1,11 +1,46 @@
 var td_proto_config = td_proto_config || {}
 
 function get_field_index_name(config, index) {
-
-    return null;
+    return (config.index_field || {})[index];
 }
 
-function get_proto_by_name(config, name) {
+function get_field_by_name(config, name) {
+    return (config.field || {})[name];
+}
 
-    return null;
+function get_field_by_index(config, index) {
+    var name = get_field_index_name(config, index)
+    if(!name) {
+        return null
+    }
+    return get_field_by_name(config, name)
+}
+
+
+function get_proto_by_name(config, name) {
+    return (config.proto || {})[name];
+}
+
+function get_proto_msg_type(config, name) {
+    return (config.msg_proto || {})[name]
+}
+
+function td_reinit_proto(config) {
+    config.field = config.field || {}
+    config.proto = config.proto || {}
+    config.index_field = {}
+    config.msg_proto = {}
+    for(var k in config.field) {
+        var value = config.field[k]
+        value.pattern = get_type_by_name(value.pattern)
+        config.index_field[value.index] = value
+    }
+
+    for(var k in config.proto) {
+        var value = config.proto[k]
+        value.pattern = get_type_by_name(value.pattern)
+        config.msg_proto[k] = value.msg_type
+    }
+
+    td_proto_config = config
 }
